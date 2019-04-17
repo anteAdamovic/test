a93f27f0ca631a13813178244d6b8bbc948ed2ff
anteAdamovic <ante.adamovic93@gmail.com>
2019-04-17 14:30:50 +0200

    Test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'test-analytics';
+
+  constructor(private location: Location, private router: Router) {
+    console.log('one', new Date().getTime());
+    const loginPaths: string[] = ["/signin", "/signup", "/forgot", "/not-found", "account/reset", "/shipping-policy", "/return-policy"];
+    if (loginPaths.find(loginPath => location.path().startsWith(loginPath))) {
+      console.log('login');
+    } else {
+      console.log('home');
+    }
+    console.log('one', new Date().getTime());
+
+    console.log('two', new Date().getTime());
+    let firstSegment = router.parseUrl(location.path()).root.children['primary'].segments[0].path;
+    let test: boolean = ["/signin", "/signup", "/forgot", "/not-found", "account/reset", "/shipping-policy", "/return-policy"].includes(firstSegment);
+    if (test) {
+      console.log('login');
+    } else {
+      console.log('home');
+    }
+    console.log('two', new Date().getTime());
+  }
 }
